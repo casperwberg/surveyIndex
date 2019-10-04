7532f07e87acb692f7e74f5906a436aa632c47e9
casperwberg <casperwberg@gmail.com>
2019-10-04 11:25:14 +0200

    redoSurveyIndex function

--- surveyIndex/R/getSurveyIdx.R
+++ surveyIndex/R/getSurveyIdx.R
@@ -306,7 +306,152 @@ getSurveyIdx <-
         totEdf=sum( unlist( lapply(zModels,getEdf))) + sum( unlist( lapply(pModels,getEdf)));
         rownames(resMat)<-yearRange
         colnames(resMat)<-ages
-        out <- list(idx=resMat,zModels=zModels,pModels=pModels,lo=loMat,up=upMat,gPreds=gPreds,logLik=logl,edfs=totEdf,pData=pData,gPreds2=gPreds2);
+        out <- list(idx=resMat,zModels=zModels,pModels=pModels,lo=loMat,up=upMat,gPreds=gPreds,logLik=logl,edfs=totEdf,pData=pData,gPreds2=gPreds2,family=famVec, cutOff=cutOff, dataAges=dataAges, yearNum=yearNum, refGear=myGear);
         class(out) <- "surveyIdx"
         out
     }
+
+
+##' Re-compute standardized survey indices for an alternative grid from a previous fitted "surveyIdx" model.
+##' 
+##' @title Re-compute standardized survey indices for an alternative grid from a previous fitted "surveyIdx" model.
+##' @param x DATRASraw dataset
+##' @param model object of class "surveyIdx" as created by "getSurveyIdx"
+##' @param predD optional DATRASraw object, defaults to NULL. If not null this is used as grid.
+##' @param myids haul.ids for grid
+##' @param nBoot number of bootstrap samples used for calculating index confidence intervals
+##' @param predfix optional named list of extra variables (besides Gear, HaulDur, Ship, and TimeShotHour),  that should be fixed during prediction step (standardized)
+##' @param mc.cores mc.cores number of cores for parallel processing
+##' @return An object of class "surveyIdx"
+##' @importFrom MASS mvrnorm
+##' @export
+redoSurveyIndex<-function(x,model,predD=NULL,myids,nBoot=1000,predfix,mc.cores=1){        
+    ages = as.numeric(colnames(model$idx))
+    dataAges <- model$dataAges
+    famVec = model$family
+    cutOff = model$cutOff
+        
+    yearNum=model$yearNum
+    yearRange=min(yearNum):max(yearNum);
+
+    gPreds=list() ##last data year's predictions
+    gPreds2=list() ## all years predictions
+    
+    myGear=model$refGear 
+    
+    resMat=matrix(NA,nrow=length(yearRange),ncol=length(ages));
+    upMat=resMat;
+    loMat=resMat;
+    do.one.a<-function(a){
+        age = which(dataAges==ages[a])
+        ddd=x[[2]]; ddd$dum=1.0;
+        ddd$A1=ddd$Nage[,age]
+        m.pos = model$pModels[[a]]
+        m0 = NULL
+        if(!famVec[a] %in% c("Tweedie","negbin")) m0 = model$zModels[[a]]
+        if(is.null(predD)) predD=subset(ddd,haul.id %in% myids);
+        res=numeric(length(yearRange));
+        lores=res;
+        upres=res;
+        gp2=list()
+        for(y in levels(ddd$Year)){
+                cat("Doing year ",y,"\n")
+                ## take care of years with all zeroes
+                if(!any(ddd$A1[ddd$Year==y]>cutOff)){
+                    res[which(as.character(yearRange)==y)]=0;
+                    upres[which(as.character(yearRange)==y)] = 0;
+                    lores[which(as.character(yearRange)==y)] = 0;
+                    next;
+                }
+
+                ## OBS: effects that should be removed should be included here
+                predD$Year=y; predD$dum=0;
+                predD$ctime=as.numeric(as.character(y));
+                predD$TimeShotHour=mean(ddd$TimeShotHour)
+                predD$Ship=names(which.max(summary(ddd$Ship)))
+                predD$timeOfYear=mean(ddd$timeOfYear);
+                predD$HaulDur=30.0
+                predD$Gear=myGear;
+                if(!is.null(predfix)){ ##optional extra variables for standardization
+                    stopifnot(is.list(predfix))
+                    for(n in names(predfix)){
+                        predD[,n] = predfix[[n]]
+                    }
+                }
+
+                p.1=try(predict(m.pos,newdata=predD,newdata.guaranteed=TRUE));
+                if(!famVec[a] %in% c("Tweedie","negbin")) p.0=try(predict(m0,newdata=predD,type="response",newdata.guaranteed=TRUE));
+                ## take care of failing predictions
+                if(!is.numeric(p.1) | (!famVec[a] %in% c("Tweedie","negbin") && !is.numeric(p.0))) {
+                    res[which(as.character(yearRange)==y)]=0;
+                    upres[which(as.character(yearRange)==y)] = 0;
+                    lores[which(as.character(yearRange)==y)] = 0;
+                    next;
+                }
+                sig2=m.pos$sig2;
+                
+                if(famVec[a]=="Gamma") { res[which(as.character(yearRange)==y)] = sum(p.0*exp(p.1)); gPred=p.0*exp(p.1) }
+                if(famVec[a]=="LogNormal")  { res[which(as.character(yearRange)==y)] = sum(p.0*exp(p.1+sig2/2)); gPred=p.0*exp(p.1+sig2/2) }
+                if(famVec[a] %in% c("Tweedie","negbin"))  { res[which(as.character(yearRange)==y)] = sum(exp(p.1)); gPred=exp(p.1) }
+                gp2[[y]]=gPred;
+                if(nBoot>10){
+                    Xp.1=predict(m.pos,newdata=predD,type="lpmatrix");
+                    brp.1=mvrnorm(n=nBoot,coef(m.pos),m.pos$Vp);
+                    if(!famVec[a] %in% c("Tweedie","negbin")){
+                        Xp.0=predict(m0,newdata=predD,type="lpmatrix");
+                        brp.0=mvrnorm(n=nBoot,coef(m0),m0$Vp);
+                        ilogit<-function(x) 1/(1+exp(-x));
+                        OS0 = matrix(0,nrow(predD),nBoot);
+                        terms.0=terms(m0)
+                        if(!is.null(m0$offset)){
+                            off.num.0 <- attr(terms.0, "offset")
+                            
+                            for (i in off.num.0) OS0 <- OS0 + eval(attr(terms.0, 
+                                                                        "variables")[[i + 1]], predD)
+                        }
+                        rep0=ilogit(Xp.0%*%t(brp.0)+OS0);
+                    }
+                    OS.pos = matrix(0,nrow(predD),nBoot);
+                    terms.pos=terms(m.pos)
+                    if(!is.null(m.pos$offset)){
+                        off.num.pos <- attr(terms.pos, "offset")
+                        
+                        for (i in off.num.pos) OS.pos <- OS.pos + eval(attr(terms.pos, 
+                                                                            "variables")[[i + 1]], predD)
+                    }
+                        
+                    
+                    if(famVec[a]=="LogNormal"){
+                        rep1=exp(Xp.1%*%t(brp.1)+sig2/2+OS.pos);
+                    } else {
+                        rep1=exp(Xp.1%*%t(brp.1)+OS.pos);
+                    } 
+
+                    if(!famVec[a] %in% c("Tweedie","negbin")){
+                        idxSamp = colSums(rep0*rep1);
+                    } else {
+                        idxSamp = colSums(rep1);
+                    }
+                    upres[which(as.character(yearRange)==y)] = quantile(idxSamp,0.975);
+                    lores[which(as.character(yearRange)==y)] = quantile(idxSamp,0.025);
+                }
+            } ## rof years
+            list(res=res,m.pos=m.pos,m0=m0,lo=lores,up=upres,gp=gPred,gp2=gp2);
+        }## end do.one
+        noAges=length(ages);
+        rr=parallel::mclapply(1:noAges,do.one.a,mc.cores=mc.cores);
+        logl=0;
+        for(a in 1:noAges){
+            resMat[,a]=rr[[a]]$res;
+            loMat[,a]=rr[[a]]$lo;
+            upMat[,a]=rr[[a]]$up;
+            gPreds[[a]]=rr[[a]]$gp;
+            gPreds2[[a]]=rr[[a]]$gp2
+        }
+        rownames(resMat)<-yearRange
+        colnames(resMat)<-ages
+        out <- list(idx=resMat,zModels=model$zModels,pModels=model$pModels,lo=loMat,up=upMat,gPreds=gPreds,logLik=model$logLik,edfs=model$edfs,pData=model$pData,gPreds2=gPreds2);
+        class(out) <- "surveyIdx"
+        out
+    }
+
