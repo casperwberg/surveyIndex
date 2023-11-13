bdeab13ac7a8409ab6e698187c9b111038b52da7
casperwberg <casperwberg@gmail.com>
2023-11-13 13:35:09 +0100

    Bump version to 1.10 bugfix: getBathyGrid now works without loading marmap package

--- surveyIndex/R/getBathyGrid.R
+++ surveyIndex/R/getBathyGrid.R
@@ -17,7 +17,7 @@ getBathyGrid<-function(d,minDepth=10, maxDepth=Inf, resolution=2,maxDist=Inf, ke
       call. = FALSE)
     }
     bathy <- marmap::getNOAA.bathy(lon1=min(d$lon),lon2=max(d$lon),lat1=min(d$lat),lat2=max(d$lat),resolution=resolution, keep=keep)
-    xyzbathy <- as.data.frame( as.xyz(bathy) )
+    xyzbathy <- as.data.frame( marmap::as.xyz(bathy) )
     colnames(xyzbathy) <- c("lon","lat","Depth")
     xyzbathy$Depth <- -xyzbathy$Depth
     xyzbathy <- subset(xyzbathy, Depth>minDepth & Depth<maxDepth)
